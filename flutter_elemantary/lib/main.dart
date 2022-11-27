2b399da187a379fd0cc338b29df945f314a125e9
Emran Deniz <63171566+EmranDenizz@users.noreply.github.com>
2022-11-28 02:41:05 +0300

    flutter exercise added.

---
+++ flutter_elemantary/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.grey[400],
+          centerTitle: true,
+          title:
+              Text('Flutter Exercises', style: TextStyle(color: Colors.black)),
+        ),
+      ),
+    );
+  }
+}
